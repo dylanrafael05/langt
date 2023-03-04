89134fc17a07cafabddc2a42e4da155666656513
Dylan Rafael <dylanrafael05@gmail.com>
2023-03-04 00:16:19 -0500

    Fixed compilation errors

--- Core/langt-core/src/Structure/Resolutions/Interfaces/ISymbol.cs
+++ Core/langt-core/src/Structure/Resolutions/Interfaces/ISymbol.cs
@@ -1,7 +1,18 @@
 namespace Langt.Structure;
 
-public interface ISymbol 
+public interface ISymbol : ISourceRanged
 {
+    /// <summary>
+    /// The symbol this instance is "based" off of.
+    /// </summary>
+    /// <remarks>
+    /// The reason for this properties' existence is to allow for
+    /// the "decoration" of a symbol (by creating a wrapping symbol)
+    /// which adds extra validation logic to the symbol's <see cref="Unravel(Context)"/>, 
+    /// like <see cref="SymbolExtensions.As{T}(ISymbol)"/>, while simultaneously
+    /// allowing inspection of the information of the symbol instance.
+    /// </remarks>
+    ISymbol BaseSymbol {get;}
     Result<object> Unravel(Context ctx);
 }
 
